9aac8c83ac93843d8853a7a4648cdbef36fbefe3
KOSASIH <kosasihg88@gmail.com>
2024-08-31 14:02:47 +0700

    Update DataServiceImpl.java

--- modules/data/DataServiceImpl.java
+++ modules/data/DataServiceImpl.java
@@ -1,16 +1,41 @@
 package com.nexusinfinitycore.modules.data;
 
-import com.nexusinfinitycore.utils.UtilsModule;
+import com.nexusinfinitycore.modules.data.DataRepository;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
-public class DataServiceImpl implements DataModule {
-    private final UtilsModule utilsModule;
+public class DataServiceImpl implements DataService {
+    private final DataRepository dataRepository;
 
-    public DataServiceImpl(UtilsModule utilsModule) {
-        this.utilsModule = utilsModule;
+    public DataServiceImpl(DataRepository dataRepository) {
+        this.dataRepository = dataRepository;
     }
 
     @Override
-    public String getData() {
-        return utilsModule.getFormattedString("Sample data from Nexus Infinity Core");
+    public CompletableFuture<String> getData() {
+        // Retrieve data from database or cache using data repository
+        CompletableFuture<String> future = new CompletableFuture<>();
+        future.completeAsync(() -> dataRepository.getData());
+        return future;
     }
-}
+
+    @Override
+    public CompletableFuture<Boolean> saveData(String data) {
+        // Save data to database or cache using data repository
+        CompletableFuture<Boolean> future = new CompletableFuture<>();
+        future.completeAsync(() -> dataRepository.saveData(data));
+        return future;
+    }
+
+    @Override
+    public List<DataEntity> getDataEntities() {
+        // Retrieve data entities from database or cache using data repository
+        return dataRepository.getDataEntities();
+    }
+
+    @Override
+    public String executeDataOperation(DataEntity dataEntity) {
+        // Execute complex data operation using data repository
+        return dataRepository.executeDataOperation(dataEntity);
+    }
+                                                           }
